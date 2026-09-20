ee2382a483108bc92d6d0984d8b240bda5c55749
its-anantagarwal <agarwal.anant.2011@gmail.com>
2026-09-20 23:11:49 +0530

    Created the intermediate scene between two levels, and the minigame timer scene
    Created the intermediate scene between two levels and wrote code to make it functional. Created a scene which is a timer for the minigames and added code for it.

---
+++ minigame_timer.gd
@@ -0,0 +1,24 @@
+extends Node2D
+@onready var timer: RichTextLabel = $timer
+
+var time : float
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	timer.text = str(snapped(time, 0.10))
+
+func Timer(start_time: float):	
+	time = start_time
+	
+	while time > 0.0:
+		await wait(0.10)
+		time = time - 0.10
+	
+	return
+	
+func wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
---
+++ timer_screen.gd
@@ -0,0 +1,60 @@
+extends Node2D
+@onready var garlic_container: HBoxContainer = $GarlicContainer
+@onready var garlic: TextureRect = $GarlicContainer/Garlic1
+@onready var garlic_2: TextureRect = $GarlicContainer/Garlic2
+@onready var garlic_3: TextureRect = $GarlicContainer/Garlic3
+@onready var garlic_4: TextureRect = $GarlicContainer/Garlic4
+@onready var garlic_5: TextureRect = $GarlicContainer/Garlic5
+@onready var level: RichTextLabel = $Level
+@onready var timer: RichTextLabel = $Timer
+
+var time
+
+func _ready() -> void:
+	await Timer(5.0)
+	
+	if Global.minigames_done < 3:
+		Global.minigames_done = Global.minigames_done +1
+		get_tree().change_scene_to_file("res://scenes/minigame_" + str(Global.minigames_done) + ".tscn")
+
+
+	else:
+		get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+	
+
+func _process(delta: float) -> void:
+	match Global.lives: 
+
+		4:
+			garlic.hide()
+		3:
+			garlic.hide()
+			garlic_2.hide()
+		2:
+			garlic.hide()
+			garlic_2.hide()
+			garlic_3.hide()
+		1:
+			garlic.hide()
+			garlic_2.hide()
+			garlic_3.hide()
+			garlic_4.hide()
+		0:
+			garlic_container.hide()
+	
+	timer.text = str(time)
+	level.text = "Level " + str(Global.minigames_done)
+
+func Timer(start_time: float):
+	time = start_time
+	
+	while time > 0.0:
+		await wait(0.1)
+		time -= 0.1
+		
+	
+	
+	return
+
+func wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
